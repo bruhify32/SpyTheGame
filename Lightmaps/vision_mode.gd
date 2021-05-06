de2f6790863f1a0c0ea3ca97ca717ede4e3c3546
AbhijitL <labhijitsingh13@gmail.com>
2021-05-06 15:46:43 +0530

    added night vission mode

--- Lightmaps/vision_mode.gd
+++ Lightmaps/vision_mode.gd
@@ -1,21 +1,28 @@
 extends CanvasModulate
 
-const DARK : Color = Color("524747");
+const DARK : Color = Color("222623");
 
 const NIGHT_VISION : Color = Color("438a50");
 
+var cooldown : bool = false;
+
+onready var timer : Timer = $Timer;
 onready var audio_stream_player : AudioStreamPlayer2D = $AudioStreamPlayer2D;
 onready var dark_mode_sound  = load("res://SFX/nightvision_off.wav");
 onready var night_vision_sound = load("res://SFX/nightvision.wav");
 
 func _ready():
+	visible = true;
 	color = DARK;
 
 func cycle_vision_mode():
-	if color == NIGHT_VISION:
-		dark_mode();
-	else:
-		night_vision_mode();
+	if not cooldown:
+		if color == NIGHT_VISION:
+			dark_mode();
+		else:
+			night_vision_mode();
+		cooldown = true;
+		timer.start();
 
 func night_vision_mode():
 	color = NIGHT_VISION;
@@ -27,3 +34,5 @@ func dark_mode():
 	audio_stream_player.stream = dark_mode_sound;
 	audio_stream_player.play();
 	
+func _on_Timer_timeout():
+	cooldown = false;
