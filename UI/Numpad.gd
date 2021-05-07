7d80f441fae5f0458834f0657f7f01972858dbe9
bruhify32 <preetileihaorambam2004@gmail.com>
2021-05-07 16:05:27 +0530

    some polish on the door lock system.

--- UI/Numpad.gd
+++ UI/Numpad.gd
@@ -6,9 +6,12 @@ onready var display : Label = $NinePatchRect/VBoxContainer/DisplayContainer/Disp
 onready var status_light : TextureRect = $NinePatchRect/VBoxContainer/ButtonContainer/GridContainer/StatusLight;
 onready var grid_container : GridContainer = $NinePatchRect/VBoxContainer/ButtonContainer/GridContainer;
 onready var audi_stream_player : AudioStreamPlayer2D = $AudioStreamPlayer2D;
+onready var timer : Timer = $Timer;
 
 onready var enter_audio = load("res://SFX/twoTone1.ogg");
 onready var ok_audio = load("res://SFX/twoTOne2.ogg");
+onready var red_dot_texture = load("res://GFX/Interface/PNG/dotRed.png");
+onready var green_dot_texture =  load("res://GFX/Interface/PNG/dotGreen.png");
 
 signal combination_correct;
 
@@ -31,8 +34,8 @@ func check_guess():
     if guess == combination:
         audi_stream_player.stream = ok_audio;
         audi_stream_player.play();
-        emit_signal("combination_correct");
-        reset_lock();
+        status_light.texture = green_dot_texture;
+        timer.start();
     else:
         reset_lock();
 
@@ -48,5 +51,10 @@ func update_display():
         check_guess();
 
 func reset_lock():
+    status_light.texture = red_dot_texture;
     display.text = "";
     guess.clear();
+
+func _on_Timer_timeout():
+    emit_signal("combination_correct");
+    reset_lock();
