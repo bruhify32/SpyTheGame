dc81ceaa5b6ffae2f64d5d2f8dde1c481ea377c3
bruhify32 <preetileihaorambam2004@gmail.com>
2021-05-07 15:51:17 +0530

    sound effects for the numpad

--- UI/Numpad.gd
+++ UI/Numpad.gd
@@ -5,6 +5,10 @@ var guess : Array = [];
 onready var display : Label = $NinePatchRect/VBoxContainer/DisplayContainer/Display;
 onready var status_light : TextureRect = $NinePatchRect/VBoxContainer/ButtonContainer/GridContainer/StatusLight;
 onready var grid_container : GridContainer = $NinePatchRect/VBoxContainer/ButtonContainer/GridContainer;
+onready var audi_stream_player : AudioStreamPlayer2D = $AudioStreamPlayer2D;
+
+onready var enter_audio = load("res://SFX/twoTone1.ogg");
+onready var ok_audio = load("res://SFX/twoTOne2.ogg");
 
 signal combination_correct;
 
@@ -25,12 +29,16 @@ func button_pressd(button : String):
 
 func check_guess():
     if guess == combination:
+        audi_stream_player.stream = ok_audio;
+        audi_stream_player.play();
         emit_signal("combination_correct");
         reset_lock();
     else:
         reset_lock();
 
 func enter(button : int):
+    audi_stream_player.stream = enter_audio;
+    audi_stream_player.play();
     guess.append(button);
     update_display();
 
